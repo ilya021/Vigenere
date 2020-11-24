fc667eb4d65d3fe87746094b19fc83395e952a9e
“Ilya” <ilya.gafarov2015@gmail.com>
2020-11-24 10:56:12 +0400

    loaded

---
+++ Vigenere/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Vigenere
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+        readonly VigenereCipher cipher = new VigenereCipher("АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ");
+        private void ButtonClickEncrypt(object sender, RoutedEventArgs e)
+        {
+            encryptedText.Text = cipher.Encrypt(inputText.Text.ToUpper(), password.Text.ToUpper());
+        }
+
+        private void ButtonClickDecrypt(object sender, RoutedEventArgs e)
+        {
+            encryptedText.Text = cipher.Decrypt(inputText.Text.ToUpper(), password.Text.ToUpper());
+        }
+    }
+}
